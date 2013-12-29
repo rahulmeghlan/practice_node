69a93dbc47563682401ef6da04f435c985186fc6
Rahul Meghlan <rahulmeghlan@gmail.com>
2013-12-29 14:54:38 +0530

    added req & res json info in files

--- practice_tcp/day2/helloWorld.js
+++ practice_tcp/day2/helloWorld.js
@@ -1,14 +1,17 @@
 (function () {
-    var fileSystem = require('fs'),
-        util = require('util'),
-        msg;
     require("http").createServer(function (req, res) {
-        fileSystem.writeFile("requestObject.js", util.inspect(req), function (err) {
-            msg = err ? err : "The file was saved successfully !!!";
-            console.log(msg);
-        });
+        writeInFile({"fileName" : "requestObject.json", "data" : req});
         res.writeHead(200, {"Content-Type": "text/plain"});
         res.end("Hello World");
+        writeInFile({"fileName" : "responseObject.json", "data" : res});
     }).listen(4000);
+
+    var writeInFile = function(fileObject){
+       var fs = require("fs"),
+            util = require('util');
+        fs.writeFile(fileObject.fileName, util.inspect(fileObject.data), function(err){
+            console.log(err ? err : "Content was successfully written !!!");
+        });
+    };
 })();
 
