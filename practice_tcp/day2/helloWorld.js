fca121878a4ef0add54d1cc5344804136e53e382
Rahul Meghlan <rahulmeghlan@gmail.com>
2013-12-29 14:40:27 +0530

    added requestObject in a file using util.inspect

--- practice_tcp/day2/helloWorld.js
+++ practice_tcp/day2/helloWorld.js
@@ -1,12 +1,12 @@
 (function () {
     var fileSystem = require('fs'),
+        util = require('util'),
         msg;
-    fileSystem.write("requestObject.txt", "test", function (err) {
-        msg = err ? err : "The file was saved successfully !!!";
-        console.log(msg);
-    });
     require("http").createServer(function (req, res) {
-
+        fileSystem.writeFile("requestObject.js", util.inspect(req), function (err) {
+            msg = err ? err : "The file was saved successfully !!!";
+            console.log(msg);
+        });
         res.writeHead(200, {"Content-Type": "text/plain"});
         res.end("Hello World");
     }).listen(4000);
